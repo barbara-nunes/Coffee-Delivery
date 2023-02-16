4163e2152cb0996ed71020c8d44322adbcc2294c
Barbara Soares <babisnunes14@gmail.com>
2023-02-16 19:14:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Home } from "./components/Home";
+import { GlobalStyle } from "./components/styles/global";
+
+export function App() {
+  return (
+
+      <div>
+        <GlobalStyle />
+        <Home />
+      </div>
+     
+    )
+}
+
